5ded75dc890080ae649785ea211cd4366e3da9a3
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-02-29 10:56:32 +0530

    final

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://starsocialmedia.herokuapp.com/',]
+ALLOWED_HOSTS = ['https://starsocialmedia.herokuapp.com/',
+                 '127.0.0.1',   
+]
 
 # Application definition
 
