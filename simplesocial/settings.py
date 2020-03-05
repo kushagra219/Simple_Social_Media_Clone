8d9b8e5bb568639c923916909c0766f6c3a7fa78
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-03-05 12:13:03 +0530

    false

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://starsocialmedia.herokuapp.com/',
                  '127.0.0.1'
